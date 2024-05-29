641303613ea64e6044cc78881df0d1d9c6d8a47b
HamidB <ejecteur@yahoo.fr>
2024-05-29 08:33:31 +0200

    Init new mod CustomFont.kt

---
+++ app/src/main/java/fr/hamtec/lesthemes/composables/CustomFont.kt
@@ -0,0 +1,14 @@
+package fr.hamtec.lesthemes.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MyCompFont() {
+    
+}
+@Preview(showBackground = true)
+@Composable
+fun MyCompFontPreview() {
+    MyCompFont()
+}
